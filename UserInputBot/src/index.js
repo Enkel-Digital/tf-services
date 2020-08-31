a05d1826ebcc5a645a5b6215d54d3a135fb1e0b1
Jaime <jaimeloeuf@gmail.com>
2020-08-31 22:17:41 +0800

    [bot] Implement DB connection and removes mock DB: 1.  Implements the interface to the actual SQL db for users. 2.  Removes the mock testing "DB" and the use of it.

--- UserInputBot/src/index.js
+++ UserInputBot/src/index.js
@@ -1,5 +1,7 @@
 require("dotenv").config();
 
+const SQLdb = require("@enkeldigital/ce-sql");
+
 const { PollingBot, shortHands } = require("yatbl");
 const bot = new PollingBot(process.env.BOT_TOKEN);
 const tapi = bot.tapi;
@@ -15,46 +17,80 @@ shortHands.setCommands(tapi, [
 
 bot.addShortHand(shortHands.replyMessage);
 
-// Simple mock DB
-const DB = {
-  chatIDs: [],
-};
-
 /**
  * Handler for start commands, where users register their
  */
-bot.onCommand("start", function (parsedCommand) {
-  // If the arguement for the first start command is null (empty) and no need to await too.
-  if (!parsedCommand[0]) return this.replyMessage("Invalid registration UUID");
+bot.onCommand("start", async function (parsedCommand, update) {
+  try {
+    const deepLinkingArgs = parsedCommand[0];
 
-  // register the user
-  // @todo Handle re-registrations, should not have double registration
-  console.log("User registration token:", parsedCommand[0][0]);
-  DB.chatIDs.push(update.message.chat.id);
-  return this.replyMessage("Successfully registered for notifications!");
+    // If the arguement for the first start command is null (empty) and no need to await too.
+    if (!deepLinkingArgs)
+      return this.replyMessage("Invalid registration link!");
+
+    // register the user
+    // @todo Handle re-registrations, should not have double registration
+    console.log("User registration token:", deepLinkingArgs[0]);
+    console.log("User's chat ID:", update.message.chat.id);
+
+    // @todo Parse and verify token before using its data
+    const token = deepLinkingArgs[0];
+
+    await SQLdb("users").insert({
+      // @todo To get this value from token too instead of hardcoding it
+      botID: 1,
+      // @todo Will be an actual token instead of just app_UUID, doing it like this for now temporarily before token logic is implemented
+      app_UUID: token,
+      t_chat_id: update.message.chat.id,
+    });
+
+    this.replyMessage("Successfully registered for notifications!");
+  } catch (error) {
+    // @todo log the error
+    console.error("Registration failed with: ", error.message);
+    return this.replyMessage("Registration failed!");
+  }
 });
 
 /**
  * Handler for unsub commands, where users request to unsubscribe for notifications
  */
-bot.onCommand("unsub", function (parsedCommand, update) {
-  // using chat id instead of from id, allow grp notifs, so like unsub from grp instead of just a user
-  console.log("unsub:", update.message.from.id);
-  console.log("unsub:", update.message.chat.id);
+bot.onCommand("unsub", async function (_, update) {
+  try {
+    // using chat id instead of from id, allow grp notifs, so like unsub from grp instead of just a user
+    console.log("unsub:", update.message.from.id);
+    console.log("unsub:", update.message.chat.id);
 
-  // Remove the user
-  const index = DB.chatIDs.indexOf(update.message.chat.id);
-  if (index > -1) {
-    DB.chatIDs.splice(index, 1);
+    // Remove the user
+    // @todo Actually when the user is unsubbing, we need to somehow figure out which bot they unsub from too, not unsub from all bots...
+    await SQLdb("users")
+      .where({
+        // @todo To get this value from token too instead of hardcoding it
+        botID: 1,
+        t_chat_id: update.message.chat.id,
+      })
+      .del();
+
     this.replyMessage("Successfully unsubscribed from notifications!");
-  } else this.replyMessage("User was not registered previously");
+
+    // Handle if user is not subbed, we should be able to tell and let them know that they were not subbed in the first place
+    // this.replyMessage("User was not registered previously");
+  } catch (error) {
+    // @todo log the error
+    console.error("Registration failed with: ", error.message);
+    return this.replyMessage("Registration failed!");
+  }
 });
 
 // Demo "notif" sending, once every 10 seconds send notif to all registered users
-setInterval(function () {
+setInterval(async function () {
   console.log("Sending new notif!");
 
-  DB.chatIDs.forEach((chatID) =>
+  const users = await SQLdb("users").select();
+
+  const chatIDs = users.map((user) => user.t_chat_id);
+
+  chatIDs.forEach((chatID) =>
     tapi("sendMessage", {
       chat_id: chatID,
       text: "Notif!",
